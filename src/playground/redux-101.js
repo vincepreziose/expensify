da5f6b8ecfbff681d73d57603b708d1655572b80
vince preziose <vince@softphusion.com>
2018-10-26 08:18:52 -0700

    wrap up redux store

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -19,6 +19,9 @@ const setCount = ( { count } = {}) => ({
   count
 });
 
+// Reducers
+// 1. Reducers are pure functions
+// 2. Never change state or action
 const countReducer = (state = { count: 0 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
